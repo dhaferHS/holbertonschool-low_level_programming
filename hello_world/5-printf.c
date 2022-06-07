c4914ecdf0aac615cc12ebdada1078220ba7b20f
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-07 14:39:49 +0200

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a fonction that prints a msg using printf
+ * Return: (0) sucesses
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\x0A");
+	return (0);
+}
