9b7143670da48cead96c8659c11873a8ff56e8e3
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-07 05:04:52 +0200

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.>
+/**
+ * prints exactly Programming is like building a multilingual puzzle, followed by a new line using the function puts
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle\");
