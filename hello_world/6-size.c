844159ef4d481901c7af3b353f7d587a6a428246
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-07 18:02:12 +0200

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main(void)
+{
+  int i;
+  clrscr();
+  printf("Size of a char  %2d byte(s) \n", sizeof(char));
+  printf("size of an int %2d byte(s) \n", sizeof(int));
+  printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+  printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+  printf("Size of a float: %d byte(s)\n", sizeof(float));
+return (0);
+}
