90e219d3a2c7004d55cf97a9b1ef2a00fe19555f
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-07 20:12:10 +0200

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-printf("Size of a char: %c byte(s)\n", sizeof(char));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("size of an int: %u byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
