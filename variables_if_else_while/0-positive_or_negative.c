4a9bb144a1bd8d836aff0c562457021cc9d3e4e0
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-08 23:24:32 +0200

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
 /**
  * main - execute file
  * Return: 0
