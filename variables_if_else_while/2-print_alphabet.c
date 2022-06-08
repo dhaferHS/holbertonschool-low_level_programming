31e8ff7c63c479ce3ef9cbed93175406a8f30942
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-09 00:46:31 +0200

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**Declare a variable with the first member of the range [a - z]
+ *
+ *Return: 0
+ */
+int main(void)
+{
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+putchar(alphabet);
+alphabet++;
+}
+putchar('\n');
+return (0);
+}
