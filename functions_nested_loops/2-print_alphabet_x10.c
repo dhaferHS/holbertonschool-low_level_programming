6ff35ddd30cf250491ea0cb6017fdd47cfacb7cd
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-09 23:58:13 +0200

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * void print_alphabet_x10 - print_alphabet
+ *
+ */
+void print_alphabet_x10(void)
+{
+    char i;
+    int n;
+    for ( n = 0 ; n < 10 ; n++ )
+    {
+    for ( i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+i++;
+_putchar('\n');
+}
+}
+}
