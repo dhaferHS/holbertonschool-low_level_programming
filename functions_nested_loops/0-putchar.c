dd914268791143994d7ed780cc8925c52870442e
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-09 22:12:50 +0200

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int _putchar(char c);
+/**
+ * main - write putchar letter by letter
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char t[] = "_putchar";
+	int i;
+	for(i =0; i<8 ;i++)
+		_putchar(t[i]);
+	_putchar('\n');
+	return(0);
+}
