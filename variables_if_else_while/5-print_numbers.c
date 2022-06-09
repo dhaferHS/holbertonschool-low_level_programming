20e999560724ef1c713eb65e074b9f6cba240891
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-09 03:58:03 +0200

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - printeg numbers from10 to 0
+ *
+ * Returen: 0
+ */
+int main(void)
+{
+	int i;
+for (i = 0; i < 10; i++)
+	printf("%d", i);
+
+printf("\n");
+return (0);
+}
