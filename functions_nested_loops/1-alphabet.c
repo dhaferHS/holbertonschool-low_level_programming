237240b43a611a85e1a8e426d42aed89517986c8
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-09 23:25:24 +0200

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void print_alphabet(void);
+/**
+ * main - Declare a variable with the first member of the range [a - z]
+ *
+ *Return: 0
+ */
+int main(void)
+{
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+putchar(alphabet);
+alphabet++;
+}
+putchar('\n');
+return (0);
+}
