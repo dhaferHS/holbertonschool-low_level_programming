9e84dc86d159d8f0977bc91d63e2cbcc60623b45
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-10 00:36:19 +0200

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower - check the code.
+ * @c: the character
+ * Return: 1 in success
+ */
+
+int _islower(int c)
+{
+if (c < 97)
+{
+return (0);
+}
+
+return (1);
+}
