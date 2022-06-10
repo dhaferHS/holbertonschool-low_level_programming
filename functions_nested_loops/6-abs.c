99c3b981d429db526f3c4bce6d989cda2c7d84b9
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-11 00:45:59 +0200

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _ads - computes the absolute value
+ *
+ * @i: the number to check
+ * Return: Always 0.
+ */
+
+int _abs(int i)
+{
+if (i >= 0)
+{
+return (i);
+}
+else
+{
+return (-i);
+}
+}
