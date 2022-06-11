f25651d00d9db77b20375b25dba227380cd70cfd
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-11 02:51:29 +0200

    8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * jack_bauer - check the code
+ *
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+int M, H;
+for (H = 0 ; H < 24  ; H++)
+{
+for (M = 0 ; M < 60 ; M++)
+{
+_putchar(H / 10 + '0');
+_putchar(H % 10 + '0');
+_putchar(M / 10 + '0');
+_putchar(M % 10 + '0');
+_putchar('\n');
+}
+}
+}
