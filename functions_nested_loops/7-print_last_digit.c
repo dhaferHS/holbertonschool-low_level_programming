2909c8ba27a97b1a860d2d621b0bfa1ee80a1291
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-11 02:04:15 +0200

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,17 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * print_last_digit - check the code
  *@n: the int number
  * 
  * Return: Always 0.
  */
-int print_last_digit(int n);
+int print_last_digit(int n)
 {
 int L;
 L = labs(n) % 10;
 _putchar('0' + L);
 _putchar('\n');
 return (L);
-
 }
