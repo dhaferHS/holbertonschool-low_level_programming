3291c027030879a0e9b1caae18a5053464a6413e
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-14 05:32:49 +0200

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower - check the code.
+ * @c: the character
+ * Return: 1 in success otherwise 0
+ */
+int _islower(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
