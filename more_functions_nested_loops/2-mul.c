9c67ece56644083444e589c2761b996e704fa2a0
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-14 06:32:23 +0200

    2-mul.c

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,8 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * mul - multiplie numbers 
- * @c: the digit
+ * mul - multiplie numbers
+ * @a: the digit
+ * @b: the digit
  * Return: 0
  */
 int mul(int a, int b)
