f4f4747a707da6517bdc997189b2f3802607af49
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-14 05:44:00 +0200

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * _islower - check the code.
+ * _isupper - check the code.
  * @c: the character
  * Return: 1 in success otherwise 0
  */
-int _islower(int c)
+int _isupper(int c)
 {
 if (c >= 65 && c <= 90)
 {
