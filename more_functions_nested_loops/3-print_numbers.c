083170a04fb91b018c50e3473deb853fd2fa7ffd
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-14 11:31:48 +0200

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - print numbers 0-9
+ *@t: the number
+ *
+ */
+void print_numbers(void)
+{
+int t = 0;
+while (t <= 9)
+{
+_putchar(t);
+t++;
+}
+_putchar('\n');
+}
