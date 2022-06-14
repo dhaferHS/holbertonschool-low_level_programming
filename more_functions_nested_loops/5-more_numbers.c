a007eafb553ef8a4360589ebb2d49798f3a9636e
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-14 16:19:36 +0200

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - print more numbers
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+char i;
+int n;
+for (n = 0 ; n < 10 ; n++)
+{
+for (i = 0; i <= 14; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
