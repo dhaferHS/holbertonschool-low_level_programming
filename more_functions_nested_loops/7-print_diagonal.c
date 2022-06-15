a894bd75da28de8231475d9b22a45035c17309a4
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-15 03:18:39 +0200

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -4,23 +4,20 @@
  * print_diagonal - print diagonal
  *
  *@n: number of lignes
- *@i: number of lignes
- *@j: number of lignes
  *
  * Return: 0
  */
 void print_diagonal(int n)
 {
 int j, i;
-for (int i = 0; i < n; i++)
+
+for (i = 0; i < n; i++)
 {
-for (int j = 0; j < n; j++)
+for (j = 0; j < i; j++)
 {
-if (j == i)
 _putchar(" ");
 }
 _putchar("\");
 _putchar("\n");
 }
-return (0);
 }
