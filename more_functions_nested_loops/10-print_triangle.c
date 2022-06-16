edc7844b84dc81505bd5e0abd43f3a5e48aea238
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-16 06:11:58 +0200

    10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,6 +9,8 @@
 void print_triangle(int size)
 {
 int i, j;
+if (size > 0)
+{
 for (i = 1; i <= size; i++)
 {
 for (j = i; j < size; j++)
@@ -20,4 +22,7 @@ _putchar('#');
 }
 _putchar('\n');
 }
+}
+else
+_putchar('\n');
 }
