4d3fb60f7564fa28931637e109388110946c28ef
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-16 04:57:40 +0200

    8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_square - print more squares
+ *
+ * @size : size squares
+ * Return: 0
+ */
+void print_square(int size)
+{
+int n, i;
+if (size > 0)
+{
+for (n = 0 ; n < size ; n++)
+{
+for (i = 0; i < size; i++)
+{
+_putchar("#");
+}
+}
+}
+else
+{
+_putchar('\n');
+}
+}
