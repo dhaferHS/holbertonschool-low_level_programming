b552a8c76ce1c863cb7c8c7440374d5d7cc62bdd
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-18 03:17:40 +0200

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 -  parameter and updates the value it points
+ *
+ * @i: a pointer
+ * Return: Always 0.
+ */
+void reset_to_98(int *i)
+{
+i = 98;
+return (0);
+}
