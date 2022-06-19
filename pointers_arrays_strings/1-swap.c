6c8d303e8a59af3a4a0c1bc8dd70220933782155
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-19 06:56:08 +0200

    1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - a parameter to swap two integers
+ *
+ * @a: variable a
+ * @b: variable b
+ * 
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b);
+{
+int i;
+i = *b;
+*b = *a;
+*a = i;
+}
