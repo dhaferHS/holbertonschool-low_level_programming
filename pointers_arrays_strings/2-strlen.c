b5fe96ea3a96d69dde9a2474183e5bc222c1c3ee
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-19 07:17:28 +0200

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - a parameter to swap two integers
+ *
+ * @s: string
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+strlen(*s);
+}
