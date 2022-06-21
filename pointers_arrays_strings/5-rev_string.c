a5652597e4b71061e3e0c5c175c15d5af08cb851
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-21 11:03:10 +0200

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - print and reverse a string
+ *
+ *@s: variable of a string
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{    
+    int i;
+    char c;
+    int len = strlen(s);
+
+    for (i=0; i< len/2; i++) {
+        c = s[i];
+        s[i] = s[len - i - 1];
+        s[len - i - 1] = c;
+    }
+}
