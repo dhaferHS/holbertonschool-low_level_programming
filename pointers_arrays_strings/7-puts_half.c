19be05a555cee9f5ba9f521a76efd1c7933890a2
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-21 14:10:45 +0200

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - check the code
+ *
+ * @s: a parameter to returns the length of a string
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+	i++;
+	s++;
+	}
+	return (i);
+}
+/**
+ * puts_half - prints half of a string
+ *
+ *@str: variable of a string
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int i;
+	int len = strlen(str);
+
+	for (i = 0; i >= (len / 2); i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
