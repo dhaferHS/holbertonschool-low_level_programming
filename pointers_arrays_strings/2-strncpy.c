dd144be5c6595868eada46a2bb1e7f892916b15e
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-23 16:46:51 +0200

    strn copy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -14,18 +14,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-		char *i;
+	int i = 0;
 
-i = dest;
-if (dest == 0)
-{
-	return (0);
-}
-while (*src && n--)
-{
-*dest = *src;
-dest++;
-src++;
-}
-return (i);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+return (dest);
+
 }
