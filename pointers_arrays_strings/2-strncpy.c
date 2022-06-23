ab85b78e66fe79e2b0a3353bddaa537de0180b27
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-23 15:28:35 +0200

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * _strncpy - copies a string
+ *
+ *
+ *
+ *@dest: string one
+ *@src: string two
+ *
+ *@n: bytes
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+*(dest + i) = *(src + i);
+dest++;
+src++;
+*dest = '\0';
+}
+return (dest);
+}
