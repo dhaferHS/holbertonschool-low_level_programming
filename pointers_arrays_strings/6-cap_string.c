f6f2dd8f38bbc0e86086b302132f3982be39a13e
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-25 04:49:49 +0200

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -15,11 +15,15 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-
-			if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] = str[i] - 32;
-			
-
+		if (i == 0)
+		{
+			if ((str[i] >= 'a' && str[i] <= 'z'))
+				str[i] = str[i] - 32;
+		}
+		if (str[i] == ' ')
+		{
+			++i;
+		}
 	}
 return (str);
 }
