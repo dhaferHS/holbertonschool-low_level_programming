2c49aa794787059fed9367952008af04c3e412c0
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-25 04:52:42 +0200

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,16 +10,14 @@
  * Return: 0
  */
 char *cap_string(char *str)
+
 {
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i == 0)
-		{
-			if ((str[i] >= 'a' && str[i] <= 'z'))
-				str[i] = str[i] - 32;
-		}
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] = str[i] - 32;
 	}
-return (str);
+	return (str);
 }
