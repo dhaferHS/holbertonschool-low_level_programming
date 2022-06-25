89e40058197ef690091887835f84c8e8ee82f1d3
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-25 05:13:31 +0200

    7-leet.c

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * leet - encode a string into 1337
+ *
+ * @str: char point
+ * Return: char
+ */
+char *leet(char *str)
+{
+	int i;
+	int a[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int n[] = {'4', '3', '0', '7', '1'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == a[i])
+			str[i] = n[i / 2];
+	}
+	return (str);
+}
+
