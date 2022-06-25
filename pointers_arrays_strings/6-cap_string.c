f086df1448ca977d30da7ecf482c831d02317c4b
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-25 04:45:14 +0200

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,3 @@
-
 #include"main.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,16 +15,13 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-
 		if (i == 0)
 		{
 			if ((str[i] >= 'a' && str[i] <= 'z'))
 				str[i] = str[i] - 32;
-			continue;
 		}
 		if (str[i] == ' ')
 		{
-
 			++i;
 			if (str[i] >= 'a' && str[i] <= 'z')
 			{
