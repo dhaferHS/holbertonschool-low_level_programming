3a8c190dca8b9db17318d355f60247aaf579fb23
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-25 04:40:00 +0200

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,43 @@
+
+#include"main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * cap_string - changes all lowercase letters of a string to uppercase
+ *@str: string
+ *
+ *
+ * Return: 0
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+
+		if (i == 0)
+		{
+			if ((str[i] >= 'a' && str[i] <= 'z'))
+				str[i] = str[i] - 32;
+			continue;
+		}
+		if (str[i] == ' ')
+		{
+
+			++i;
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+				continue;
+			}
+		}
+		else
+		{
+			if (str[i] >= 'A' && str[i] <= 'Z')
+				str[i] = str[i] + 32;
+		}
+	}
+return (str);
+}
