14f1bbf9fc6e3af31afcc077d5d40d7178d5fd26
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-26 22:22:51 +0200

    main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -22,4 +22,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *);
 char *leet(char *str);
+char *_memset(char *s, char b, unsigned int n);
 #endif /* MAIN_H */
