f25f1bd55cbc2e87ff46305d3277a915ff39c822
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-27 11:59:56 +0100

    2-strchr.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr -  locates a character in a string
+ *
+ * @c: char point
+ * @s: char
+ *
+ * Return: char
+ */
+char *_strchr(char *s, char c)
+{
+	char *position = NULL;
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			position = &s[i];
+			break;
+		}
+	}
+	return (position);
+}
