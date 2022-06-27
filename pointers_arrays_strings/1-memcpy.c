435c9a93209f82a93a1c1cbd927308fd0c735800
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-27 10:14:07 +0200

    1-memcpy.c

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memcpy - copies memory from a memory area to an other area
+ *
+ * @dest: char point
+ * @src: char
+ * @n: number of bytes
+ *
+ * Return: char
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+	src[i] = dest[i];
+}
+return (dest);
+}
