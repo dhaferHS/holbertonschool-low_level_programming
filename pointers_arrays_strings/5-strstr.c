064968a16e95202a31f16beea9207ccc3d2ccf62
dhaferHS <dhafer.darkprod@gmail.com>
2022-06-29 14:39:31 +0100

    5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
 /**
  * _strpbrk - searches a string for any of a set of bytes
  * @haystack: string one
@@ -9,23 +9,25 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-int i, j, k, count = 0;
+	int i, j, k, count = 0;
 
-for (i = 0; needle[i] != '\0'; i++)
-{
-}
-for (j = 0 ; haystack[j]!='\0'; j++)
-{
-if (haystack[j] == needle[0])
-{
-for (k = 0 ; k < i;k++)
-{
-if (haystack[j]==needle[k]){
-j++;
-count++;
-}
-}
-}
-}
-return (haystack + (i + 1));
+	for (i = 0; needle[i] != '\0'; i++)
+	{
+
+		for (j = 0; haystack[j] != '\0'; j++)
+		{
+			if (haystack[j] == needle[0])
+			{
+				for (k = 0; k < i; k++)
+				{
+					if (haystack[j] == needle[k])
+					{
+						j++;
+						count++;
+					}
+				}
+			}
+		}
+	}
+	return (haystack + (i + 1));
 }
