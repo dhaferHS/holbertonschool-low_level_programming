7151e8a9603320fc0925c15dddde8510a1c15aea
dhaferHS <dhafer.darkprod@gmail.com>
2022-07-01 13:53:35 +0100

    3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * factorial - print factorial
+ * @s: string
+ *
+ * Return: 0
+ */
+int factorial(int n)
+{
+if (n < 0)
+	return (-1);
+if (n == 0)
+	return (1);
+return(n * factorial(n - 1));
+}
