85e631ccc07c7aabfe035af04f10673b11be9b2a
dhaferHS <dhafer.darkprod@gmail.com>
2022-07-01 11:38:40 +0100

    1-print_rev_recursion.c

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _print_rev_recursion - print in rev
+ * @s: string
+ *
+ * Return: 0
+ */
+void _print_rev_recursion(char *s)
+{
+		if (*s == '\0')
+	{
+	return;
+	}
+_print_rev_recursion(s + 1);
+_putchar(*s);
+
+}
+
