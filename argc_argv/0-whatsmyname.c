df5e35014e0cf858afbb21b30902060b269b2ae1
dhaferHS <dhafer.darkprod@gmail.com>
2022-07-03 21:12:58 +0100

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - a program that print the name of the files followed by a new name
+ * 
+ * @argc: argc 
+ * @argv: argv 
+ * return: void
+ */
+ int main(int argc, char **argv)
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
