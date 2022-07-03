49b8533ba877a4e1735a9d297ee73f54abad821a
dhaferHS <dhafer.darkprod@gmail.com>
2022-07-03 21:23:56 +0100

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main- program that print the name of the files followed by a new name
+ *
+ * @argc: argc
+ * @argv: argv
+ * Return: void
+ */
+int main(int argc, char **argv)
+{
+(void)argv;
+printf("%d\n", argc);
+return (0);
+}
