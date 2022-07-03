eafa8e48cacc038b05dffd4b7334e92d83dac0bd
dhaferHS <dhafer.darkprod@gmail.com>
2022-07-03 23:15:55 +0100

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,12 +11,12 @@ int main(int argc, char **argv)
 {
 	int a, b;
 
-if ((argc !='\0') && (argc == 3))
+if ((argc != '\0') && (argc == 3))
 {
 a = atoi(argv[1]);
 b = atoi(argv[2]);
 
-printf("%d\n", a * b);
+printf("%d\n", a *b);
 }
 else
 {
