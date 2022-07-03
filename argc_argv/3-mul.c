5cf1642f0573d69a0a36db78f035c6a2fd103639
dhaferHS <dhafer.darkprod@gmail.com>
2022-07-03 22:55:43 +0100

    3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main- program that prints the number of arguments passed into it
+ *
+ * @argc: argc
+ * @argv: argv
+ * Return: void
+ */
+int main(int argc, char **argv)
+{
+	int i, a, b, sum;
+(void)argv;
+if (argc[i] !='\0')
+{
+argc[1] == a;
+argc[2] == b;
+sum = a +b;
+printf("%d\n", sum)
+return (0);
+}
+else
+{
+		printf(Error).
+	return (1);
+}
+}
