84c69a7b073f53617d0fe28e95961dedf6627d36
dhaferHS <dhafer.darkprod@gmail.com>
2022-07-03 23:46:36 +0100

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main- program that prints the number of arguments passed into it
+ *
+ * @argc: argc
+ * @argv: argv
+ * Return: void
+ */
+int main(int argc, char **argv)
+{
+	int x, y, t;
+for (x = 0; x < argc; x++)
+{
+	for (y = 0; argv[x][y]; y++)
+	{
+	
+		if (argv[x][y] < 48 || argv[x][y] > 57)
+	{		
+		printf("Error\n");
+	return (1);
+	}
+	t += atoi(argv[x]);
+}
+return (0);
+}
