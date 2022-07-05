6ab5f0096271fc4c1d8b012a5025049fae44cfb0
dhaferHS <dhafer.darkprod@gmail.com>
2022-07-05 20:21:35 +0100

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef _DOG_H_
 #define _DOG_H_
-
+/**
+ * struct dog - tell the name and the age
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: name of the owner
+ *
+ */
 struct dog
 {
 char *name;
