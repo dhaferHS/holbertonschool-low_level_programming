2bfbe1688aa125598a4aeed1e560b4582529f7f8
dhaferHS <dhafer.darkprod@gmail.com>
2022-07-06 09:40:46 +0100

    2-int_index.c

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"function_pointers.h"
+
+/**
+ * int_index -  returns the index of the first element
+ *@array: array
+ *
+ * @size: size of the array
+ * @cmp: pointer
+ *
+ * Return: 0 or 1 or -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!array || !cmp || size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]) != 0)
+			return (i);
+	return (-1);
+
+}
