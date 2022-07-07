9a21009335ed54062f7d21758b55156eef903f57
dhaferHS <dhafer.darkprod@gmail.com>
2022-07-07 16:13:25 +0100

    0-sum_them_all.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -18,8 +18,7 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 
-		
-	va_start(args, n);
+va_start(args, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
 	va_end(args);
