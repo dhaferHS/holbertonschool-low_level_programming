999a50b4958d973c10fcc826677a02727c6751d1
dhaferHS <dhafer.darkprod@gmail.com>
2022-07-07 17:55:38 +0100

    2-print_strings.c

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints string
+ * @n: number
+ * @separator: pointer of the string
+ * Return: the sum
+ *
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list args;
+
+	char *STRING;
+	
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		STRING = va_arg(args, char*);
+
+
+		if (STRING == NULL)
+			printf("%s", "(nil)");
+		else
+
+			printf("%s", STRING);
+
+		if ((separator != NULL) && (i != (n - 1)))
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
