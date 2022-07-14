a29686ccd32cd3e05c96fa5efadd7fdd904e4ad5
dhaferHS <dhafer.darkprod@gmail.com>
2022-07-14 21:26:50 +0100

    0-create_array.c

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include"main.h"
+/**
+ * create_array - creat an array of chars 
+ * @size: size of the array
+ * @c: char
+ * 
+ * Return: 0
+ * 
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	i = 0;
+	if (size == 0)
+		return (NULL);
+	arr = malloc(size * sizeof(char));
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	while (i < size)
+	{
+		*(arr + i) = c;
+		i++;
+	}
+	return (arr);
+}
