7ba407495ea734a76a61b7e579114b303ff65d6e
dhaferHS <dhafer.darkprod@gmail.com>
2022-07-14 21:48:52 +0100

    1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strdup - function that returns a pointer to newly
+ *
+ * @str: char
+ *
+ * Return: Nothing
+ */
+char *_strdup(char *str)
+{
+
+	char *a, *b;
+	int size = strlen(str);
+
+	if (str == NULL)
+		return (NULL);
+	a = malloc(sizeof(char) * (size));
+	if (a == NULL)
+		return (NULL);
+
+	b = a;
+
+	while (*str)
+		*b++ = *str++;
+
+	return (a);
+
+}
