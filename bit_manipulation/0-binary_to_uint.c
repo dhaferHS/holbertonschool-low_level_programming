6f68160f726ba4ef8084619b96e0fda74b25ed2e
dhaferHS <dhafer.darkprod@gmail.com>
2022-07-20 04:27:36 +0200

    0-binary_to_uint.c

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
  * binary_to_uint - function that converts a binary number to an unsigned int
  * @b: sring
  *
- * Return : 0
+ * Return: converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
