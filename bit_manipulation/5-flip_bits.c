afb8458761f8d489f57b2f9ff78888fa5b15a9b7
dhaferHS <dhafer.darkprod@gmail.com>
2022-07-20 06:51:01 +0200

    5-flip_bits.c

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -7,19 +7,20 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    int flips = 0;
+int flips = 0;
 
-      while(n > 0 || m > 0)
-      {
+while (n > 0 || m > 0)
+{
+
+int t1 = (n & 1);
+int t2 = (m & 1);
   
-    int t1 = (n&1);
-    int t2 = (m&1);
-  
-    if(t1!=t2){
+if(t1 != t2)
+{
       flips++;
-    }
-    n>>=1;
-    m>>=1;
+}
+    n >>= 1;
+    m >>= 1;
   }
   return (flips);
 }
