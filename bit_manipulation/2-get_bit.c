129976f822a461e2cac2c2d45fc320515a496b19
dhaferHS <dhafer.darkprod@gmail.com>
2022-07-20 05:38:24 +0200

    2-get_bit.c

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * get_bit - function that returns the value of a bit at a given index
+ * @n: unsigned long int
+ * @index:  value of the bit
+ * Return:  value of the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (index> (sizeof(unsigned long int ) * 8))
+return(-1);
+return (1 == ((n >> index) & 1));
+}
