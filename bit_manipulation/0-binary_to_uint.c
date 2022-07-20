0b76246e97798139834ba26617483c992c848554
dhaferHS <dhafer.darkprod@gmail.com>
2022-07-20 04:09:37 +0200

    0-binary_to_uint.c

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,28 +1,28 @@
 #include"main.h"
 /**
  * binary_to_uint - converts a binary number to an unsigned int
- * @b: sring 
- * 
+ * @b: sring
+ *
  * Return : converted number, or 0
  */
-unsigned int binary_to_uint( const char *b)
+unsigned int binary_to_uint(const char *b)
 {
  unsigned int k = 1;
- unsigned int i=0;
+ unsigned int i = 0;
  int c;
  unsigned int len;
 
- len = strlen(b);
+len = strlen(b);
 
-  for (c = len-1; c >= 0; c--)
- {
-  if (b[c] != '0' && b[c] != '1')
-      return (0);
-    if (b[c] == '1')
-    {
-     i += k;
-     }
-    k *= 2;
+for (c = len-1; c >= 0; c--)
+{
+if (b[c] != '0' && b[c] != '1')
+return (0);
+if (b[c] == '1')
+{
+i += k;
 }
- return (i);
+k *= 2;
+}
+return (i);
 }
