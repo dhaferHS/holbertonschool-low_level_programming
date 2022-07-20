b3380d1dd36c78dd367619f577958c208bbac813
dhaferHS <dhafer.darkprod@gmail.com>
2022-07-20 05:39:10 +0200

    2-get_bit.c

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -7,7 +7,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-if (index> (sizeof(unsigned long int ) * 8))
-return(-1);
+if (index > (sizeof(unsigned long int) * 8))
+return (-1);
 return (1 == ((n >> index) & 1));
 }
