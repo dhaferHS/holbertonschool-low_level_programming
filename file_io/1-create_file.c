d5a1bff7e48f3931342955376b316843ba9fcb49
dhaferHS <dhafer.darkprod@gmail.com>
2022-07-21 07:12:24 +0200

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 
     if (filename == NULL)
         return (-1);
-    file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+    file = open(filename, O_CREAT | O_TRUNC | O_WRONLY);
     if (file == -1)
         return (-1);
 
